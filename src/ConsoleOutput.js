ad8f17bc5401406ede8c6d8bac380f146fb837e3
Cheng Qiu <chengqiu220@gmail.com>
2022-07-08 14:51:36 -0400

    Initial commit

---
+++ src/ConsoleOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ConsoleOutput = (props) => {
+  return(
+    <>
+      <div>Hello world its me</div>
+      <div>{props.message}</div>
+    </>
+  )
+}
+
+export default ConsoleOutput;
